c5b5f08066a11719833a65b84ab9f8c6216a99a0
cristian <crisbed321@gmail.com>
2020-07-20 23:04:30 +0000

    fix holberton.h

--- holberton.h
+++ holberton.h
@@ -4,23 +4,24 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+/**
+ * struct format - Struct op
+ * @type: The operator
+ * @j: The function associated
+ */
+typedef struct format
+{
+	char *type;
+	int (*func_fmt)();
+} format_t;
+
 int _printf(const char *format, ...);
-int (*_getmatch_print(char *s))(va_list);
+int (*_getmatch_print(const char type))();
 int _printChar(va_list c);
 int _printString(va_list s);
 int _printInt(va_list i);
 int _strcmp(char *s1, char *s2);
 int _putchar(char c);
 
-/**
- * struct format - Struct op
- * @op: The operator
- * @j: The function associated
- */
-typedef struct format
-{
-	char *op;
-	int (*j)(va_list);
-} format_t;
 
 #endif /* #ifndef PRINTF_H */
